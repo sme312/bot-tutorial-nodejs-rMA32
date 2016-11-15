c9b85e1f20941272715f9e45e4f6fda0da0fbab3
sme312 <smevans312@gmail.com>
2016-11-15 16:00:11 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ botTrigger = /(.|)*Show me the survey/;
 if(request.text && botTrigger.test(request.text)) {
 this.res.writeHead(200);
 postMessage("https://www.surveymonkey.com/r/ZFGTZCC");
-//postMessage("https://www.surveymonkey.com/r/B6PB5TR");
+postMessage("https://www.surveymonkey.com/r/B6PB5TR");
 //postMessage("No survey :^(");
 this.res.end();
 }
